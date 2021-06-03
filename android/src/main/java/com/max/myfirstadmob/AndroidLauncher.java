eb19a5692cc34fa5a239707e8ea468f0603cfe09
maxkcy <54729511+maxkcy@users.noreply.github.com>
2021-06-03 13:46:13 -0700

    ad displayed

--- android/src/main/java/com/max/myfirstadmob/AndroidLauncher.java
+++ android/src/main/java/com/max/myfirstadmob/AndroidLauncher.java
@@ -1,9 +1,19 @@
 package com.max.myfirstadmob;
 
 import android.os.Bundle;
+import android.view.View;
+import android.view.Window;
+import android.view.WindowManager;
+import android.widget.RelativeLayout;
 
 import com.badlogic.gdx.backends.android.AndroidApplication;
 import com.badlogic.gdx.backends.android.AndroidApplicationConfiguration;
+import com.google.android.gms.ads.AdRequest;
+import com.google.android.gms.ads.AdSize;
+import com.google.android.gms.ads.AdView;
+import com.google.android.gms.ads.MobileAds;
+import com.google.android.gms.ads.initialization.InitializationStatus;
+import com.google.android.gms.ads.initialization.OnInitializationCompleteListener;
 import com.max.myfirstadmob.MyFirstAdMobMain;
 
 /** Launches the Android application. */
@@ -11,7 +21,44 @@ public class AndroidLauncher extends AndroidApplication {
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
+		
+		MobileAds.initialize(this, new OnInitializationCompleteListener() {
+			@Override
+			public void onInitializationComplete(InitializationStatus initializationStatus) {
+			}
+		});
+
 		AndroidApplicationConfiguration configuration = new AndroidApplicationConfiguration();
-		initialize(new MyFirstAdMobMain(), configuration);
+		//initialize(new MyFirstAdMobMain(), configuration);
+
+		RelativeLayout layout = new RelativeLayout(this);
+
+		requestWindowFeature(Window.FEATURE_NO_TITLE);
+		getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN,
+				WindowManager.LayoutParams.FLAG_FULLSCREEN);
+		getWindow().clearFlags(WindowManager.LayoutParams.FLAG_FORCE_NOT_FULLSCREEN);
+
+		View gameView = initializeForView(new MyFirstAdMobMain(), configuration);
+
+		AdView adView = new AdView(this);
+		adView.setAdSize(AdSize.BANNER);
+		adView.setAdUnitId("ca-app-pub-3940256099942544/6300978111");
+		//adView.setAdUnitId(""); // Put in your secret key here
+
+		AdRequest adRequest = new AdRequest.Builder().build();
+		adView.loadAd(adRequest);
+
+		layout.addView(gameView);
+
+		RelativeLayout.LayoutParams adParams =
+				new RelativeLayout.LayoutParams(RelativeLayout.LayoutParams.WRAP_CONTENT,
+						RelativeLayout.LayoutParams.WRAP_CONTENT);
+		adParams.addRule(RelativeLayout.ALIGN_PARENT_BOTTOM);
+		adParams.addRule(RelativeLayout.CENTER_HORIZONTAL);
+
+		layout.addView(adView, adParams);
+
+		setContentView(layout);
+
 	}
 }
