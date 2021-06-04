9c0b367efc2c401925e81089f20b51c4f8b7287a
maxkcy <54729511+maxkcy@users.noreply.github.com>
2021-06-04 16:23:37 -0700

    full day with linear layout

--- android/src/main/java/com/max/myfirstadmob/AndroidLauncher.java
+++ android/src/main/java/com/max/myfirstadmob/AndroidLauncher.java
@@ -1,11 +1,18 @@
 package com.max.myfirstadmob;
 
+
 import android.os.Bundle;
+import android.os.Handler;
+import android.os.Message;
+import android.util.DisplayMetrics;
 import android.view.View;
+import android.view.ViewGroup;
 import android.view.Window;
 import android.view.WindowManager;
-import android.widget.RelativeLayout;
+import android.widget.LinearLayout;
 
+
+import com.badlogic.gdx.Gdx;
 import com.badlogic.gdx.backends.android.AndroidApplication;
 import com.badlogic.gdx.backends.android.AndroidApplicationConfiguration;
 import com.google.android.gms.ads.AdRequest;
@@ -14,11 +21,42 @@ import com.google.android.gms.ads.AdView;
 import com.google.android.gms.ads.MobileAds;
 import com.google.android.gms.ads.initialization.InitializationStatus;
 import com.google.android.gms.ads.initialization.OnInitializationCompleteListener;
-import com.max.myfirstadmob.MyFirstAdMobMain;
+
 
 /** Launches the Android application. */
-public class AndroidLauncher extends AndroidApplication {
+public class AndroidLauncher extends AndroidApplication implements IActivityRequestHandler {
+	public AdView adView;
+
+	private final int SHOW_ADS = 1;
+	private final int HIDE_ADS = 0;
+
+	protected Handler handler = new Handler()
+	{
+		@Override
+		public void handleMessage(Message msg) {
+			switch(msg.what) {
+				case SHOW_ADS:
+				{
+					adView.setVisibility(View.VISIBLE);
+					break;
+				}
+				case HIDE_ADS:
+				{
+					adView.setVisibility(View.GONE);
+					break;
+				}
+			}
+		}
+	};
+
+
 	@Override
+	public void showAds(boolean show) {
+		handler.sendEmptyMessage(show ? SHOW_ADS : HIDE_ADS);
+	}
+
+
+		@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		
@@ -31,16 +69,18 @@ public class AndroidLauncher extends AndroidApplication {
 		AndroidApplicationConfiguration configuration = new AndroidApplicationConfiguration();
 		//initialize(new MyFirstAdMobMain(), configuration);
 
-		RelativeLayout layout = new RelativeLayout(this);
+		//RelativeLayout layout = new RelativeLayout(this);
+		LinearLayout layout = new LinearLayout(this);
+		layout.setOrientation(LinearLayout.VERTICAL);
 
 		requestWindowFeature(Window.FEATURE_NO_TITLE);
 		getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN,
 				WindowManager.LayoutParams.FLAG_FULLSCREEN);
 		getWindow().clearFlags(WindowManager.LayoutParams.FLAG_FORCE_NOT_FULLSCREEN);
 
-		View gameView = initializeForView(new MyFirstAdMobMain(), configuration);
+		View gameView = initializeForView(new MyFirstAdMobMain(this), configuration);
 
-		AdView adView = new AdView(this);
+		adView = new AdView(this);
 		adView.setAdSize(AdSize.BANNER);
 		adView.setAdUnitId("ca-app-pub-3940256099942544/6300978111");
 		//adView.setAdUnitId(""); // Put in your secret key here
@@ -48,17 +88,31 @@ public class AndroidLauncher extends AndroidApplication {
 		AdRequest adRequest = new AdRequest.Builder().build();
 		adView.loadAd(adRequest);
 
-		layout.addView(gameView);
+			DisplayMetrics displaymetrics = new DisplayMetrics(); getWindowManager().getDefaultDisplay().getMetrics(displaymetrics);
+			int height = displaymetrics.heightPixels;  int width = displaymetrics.widthPixels;
 
-		RelativeLayout.LayoutParams adParams =
+			ViewGroup.LayoutParams adParams =
+					new LinearLayout.LayoutParams(LinearLayout.LayoutParams.WRAP_CONTENT, LinearLayout.LayoutParams.WRAP_CONTENT);
+			adParams.height = height - (adView.getAdSize().getHeight() + 40);
+			adParams.width = width;
+		layout.addView(gameView, adParams);
+
+		/*RelativeLayout.LayoutParams adParams =
 				new RelativeLayout.LayoutParams(RelativeLayout.LayoutParams.WRAP_CONTENT,
 						RelativeLayout.LayoutParams.WRAP_CONTENT);
 		adParams.addRule(RelativeLayout.ALIGN_PARENT_BOTTOM);
-		adParams.addRule(RelativeLayout.CENTER_HORIZONTAL);
+		adParams.addRule(RelativeLayout.CENTER_HORIZONTAL);*/
 
-		layout.addView(adView, adParams);
+		ViewGroup.LayoutParams adParams2 =
+				new LinearLayout.LayoutParams(LinearLayout.LayoutParams.WRAP_CONTENT, LinearLayout.LayoutParams.WRAP_CONTENT);
+			adParams2.height = adView.getAdSize().getHeight() + 40;
+
+			adParams2.width = width;
+		layout.addView(adView, adParams2);
 
 		setContentView(layout);
 
-	}
+
+		}
+
 }
